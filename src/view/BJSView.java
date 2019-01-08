696791462c649de537ea2b44c03af871cdec1ddd
rafaelbraga25 <rafael.braga.gc@gmail.com>
2019-01-08 10:17:05 +0000

    Add tests

--- src/view/BJSView.java
+++ src/view/BJSView.java
@@ -1,11 +1,7 @@
 package view;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public final class BJSView {
@@ -14,7 +10,6 @@ public final class BJSView {
 
     public BJSView() {
         menus = new HashMap<>();
-
         menus.put(0, new Menu("Choose an option...",
                 Arrays.asList(new Option("Run all tests -", "1"),
                         new Option("Run one test --", "2"),
@@ -22,38 +17,11 @@ public final class BJSView {
         );
     }
 
-    public void displaySpacing() {
-        System.out.println("\n");
-    }
-
     public void displayMessage(final String message) {
         System.out.print(message);
     }
 
-    public void displayLocalDate(final LocalDate date, final DateTimeFormatter formatter) {
-        System.out.println(date.format(formatter));
-    }
-
-    public void displayLocalDateTime(final LocalDateTime datetime, final DateTimeFormatter formatter) {
-        System.out.print(datetime.format(formatter));
-    }
-
-    public void displayPage(final List<String> elements, final int currentPage, final int totalPages) {
-        this.displaySpacing();
-
-        int i = 1;
-
-        for(String element : elements) {
-            System.out.println(element + " " + i);
-            i++;
-        }
-
-        System.out.println("Page " + currentPage + " of " + totalPages);
-    }
-
     public void displayMenu(final int menu) {
-        this.displaySpacing();
-
         if (menus.containsKey(menu)) {
             this.menus.get(menu).show();
         }
