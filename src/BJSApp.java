de469ccf35fe91bd622919930f5851d3bf276ad6
rafaelbraga25 <rafael.braga.gc@gmail.com>
2019-01-08 12:32:32 +0000

    Organize project

--- src/BJSApp.java
+++ src/BJSApp.java
@@ -1,8 +1,7 @@
-import controller.BJSController;
 import model.BJSModel;
 import model.BJSUtils;
+import model.Crono;
 import model.Transaction;
-import view.BJSView;
 
 import java.time.DayOfWeek;
 import java.time.Month;
@@ -121,12 +120,52 @@ public class BJSApp {
 
     public static void main(String[] args) {
         //tests();
-        BJSView view = new BJSView();
-        BJSModel model = new BJSModel();
-        BJSController controller = new BJSController();
+        Crono.start();
+        List<List<Transaction>> transactions = Arrays.asList(
+                BJSUtils.load("transCaixa1M.txt", BJSUtils.parseTransaction),
+                BJSUtils.load("transCaixa2M.txt", BJSUtils.parseTransaction),
+                BJSUtils.load("transCaixa4M.txt", BJSUtils.parseTransaction),
+                BJSUtils.load("transCaixa6M.txt", BJSUtils.parseTransaction));
+        System.out.println("Files loaded in " + Crono.stop() + " seconds");
 
-        controller.setView(view);
-        controller.setModel(model);
-        controller.startFlow();
+        final BJSModel model = new BJSModel();
+
+        System.out.println("Starting test 1");
+        model.t1(transactions);
+        System.out.println("Test 1 done");
+        System.out.println("Starting test 2");
+        model.t2(transactions);
+        System.out.println("Test 2 done");
+        System.out.println("Starting test 3");
+        model.t3();
+        System.out.println("Test 3 done");
+        System.out.println("Starting test 4");
+        model.t4(transactions);
+        System.out.println("Test 4 done");
+        System.out.println("Starting test 5");
+        model.t5(transactions);
+        System.out.println("Test 5 done");
+        System.out.println("Starting test 6");
+        model.t6(transactions);
+        System.out.println("Test 6 done");
+        System.out.println("Starting test 7");
+        model.t7(transactions);
+        System.out.println("Test 7 done");
+        System.out.println("Starting test 8");
+        model.t8(transactions);
+        System.out.println("Test 8 done");
+        System.out.println("Starting test 9");
+        model.t9(transactions);
+        System.out.println("Test 9 done");
+        System.out.println("Starting test 10");
+        model.t10(transactions);
+        System.out.println("Test 10 done");
+        System.out.println("Starting test 11");
+        model.t11(transactions);
+        System.out.println("Test 11 done");
+        System.out.println("Starting test 12");
+        model.t12(transactions);
+
+        System.out.println("All done");
     }
 }
